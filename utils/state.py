66e702f020c1144ec6a1d8864c8dcf0e0b56bc1d
LesterSUN <a199961@siswa.ukm.edu.my>
2026-01-20 21:31:22 +0800

    Update state.py

--- utils/state.py
+++ utils/state.py
@@ -1,1 +1,15 @@
 
+import streamlit as st
+
+def init_state():
+    if "token" not in st.session_state:
+        st.session_state.token = None
+    if "role" not in st.session_state:
+        st.session_state.role = None
+    if "username" not in st.session_state:
+        st.session_state.username = None
+
+def logout():
+    st.session_state.token = None
+    st.session_state.role = None
+    st.session_state.username = None

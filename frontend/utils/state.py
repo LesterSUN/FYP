98faef6d3595ad3f7680f776598613551a3f1490
LesterSUN <a199961@siswa.ukm.edu.my>
2026-01-21 09:09:51 +0800

    Update state.py

--- frontend/utils/state.py
+++ frontend/utils/state.py
@@ -2,12 +2,9 @@
 import streamlit as st
 
 def init_state():
-    if "token" not in st.session_state:
-        st.session_state.token = None
-    if "role" not in st.session_state:
-        st.session_state.role = None
-    if "username" not in st.session_state:
-        st.session_state.username = None
+    st.session_state.setdefault("token", None)
+    st.session_state.setdefault("role", None)
+    st.session_state.setdefault("username", None)
 
 def logout():
     st.session_state.token = None
